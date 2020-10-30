8ffd732a943405808c8a60e1c99410a43131cbcf
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-10-31 04:27:11 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace LinkedListDataStructure
 {
@@ -9,33 +9,45 @@ namespace LinkedListDataStructure
             Console.WriteLine("Welcome To Linked List Data Structure!");
             //Creating object of linked list
             LinkedList linkedList = new LinkedList();
-            //adding data
-            //linkedList.DataAtEnd(56);
-            //linkedList.DataAtEnd(30);
-            //linkedList.DataAtEnd(70);
-            linkedList.DisplayLinkedList();
+            //sortedLinkedList
+            /* Console.WriteLine("Sorted Linked List");
+             linkedList.SortedLinkedList(56);
+             linkedList.SortedLinkedList(30);
+             linkedList.SortedLinkedList(70);
+             linkedList.SortedLinkedList(4);
+             linkedList.SortedLinkedList(100);
+             linkedList.SortedLinkedList(34);
+             linkedList.SortedLinkedList(44);
+              linkedList.DisplayLinkedList();*/
+            Console.WriteLine("****************************************************");
             //adding data in front of linkedlist
-            linkedList.DataAtFront(70);
-            linkedList.DataAtFront(30);
-            linkedList.DataAtFront(56);
+            Console.WriteLine("LinkedList");
+            linkedList.DataAtFrontInLinkedList(56);
+            linkedList.DataAtFrontInLinkedList(30);
+            linkedList.DataAtFrontInLinkedList(70);
             //displaying linkedlist
             linkedList.DisplayLinkedList();
-            //inserting element in linkedlist
-            linkedList.InsertingData(4, 45);
-            linkedList.DisplayLinkedList();
+            Console.WriteLine("****************************************************");
+            //inserting element in linkedlist specifying position and data
+            /* linkedList.InsertingDataInLinkedList(4, 45);
+             linkedList.DisplayLinkedList();*/
             //deleting 1st element from list
-            //linkedList.DeleteFirstElement();
+            //linkedList.DeleteFirstElementInLinkedList();
             //linkedList.DisplayLinkedList();
             //deleting last element
-            //linkedList.DeleteLastElement();
+            //linkedList.DeleteLastElementInLinkedList();
             //linkedList.DisplayLinkedList();
             //searching data in the list
-            //linkedList.SearchLinkedList(56);
-            int searchdata = 45;
-            int index = linkedList.SearchLinkedList(searchdata);
-            Console.WriteLine($"{searchdata} is present in the list at {index} position.");
-            //inserting element in list using data
-            linkedList.InsertingElementWithData(16, 45);
+            //linkedList.SearchingList(56);
+            int searchdata = 30;
+            int index = linkedList.SearchingList(searchdata);
+            Console.WriteLine("\nSearching for element in linked list");
+            if (index == 0)
+                Console.WriteLine($"{searchdata} is not in the list");
+            else
+                Console.WriteLine($"{searchdata} is present in the list at {index} position.");
+            //inserting element in list using data of previous element
+            linkedList.InsertingElementWithDataInLinkedList(16, 70);
             linkedList.DisplayLinkedList();
             //deleting specific element from the list
             linkedList.DeleteSpecificElementFromList(30);
